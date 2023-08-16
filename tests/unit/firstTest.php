4fe620b6082f88c6cbe2620fa22c8ce1779f541d
yanliu1111 <liuyan0504@gmail.com>
2023-08-16 08:43:48 -0600

    feat: @test

--- tests/unit/firstTest.php
+++ tests/unit/firstTest.php
@@ -4,8 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 class firstTest extends TestCase
 {
-    public function testMultiplicationTwoNumbers()
+    /** @test */
+    public function MultiplicationTwoNumbers()
     {
         $this->assertEquals(4, 2 * 2);
     }
+
+   
 }
