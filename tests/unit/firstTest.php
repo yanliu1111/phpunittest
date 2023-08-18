6570c98625f4c670602bdfcbb268d0ee580c8eee
yanliu1111 <liuyan0504@gmail.com>
2023-08-18 07:49:07 -0600

    feat: test

--- tests/unit/firstTest.php
+++ tests/unit/firstTest.php
@@ -4,10 +4,14 @@ use PHPUnit\Framework\TestCase;
 
 class firstTest extends TestCase
 {
-    /** @test */
-    public function MultiplicationTwoNumbers()
+    public function testMultiplicationTwoNumbers()
     {
-        $this->assertEquals(4, 2 * 2);
+        $output = false;
+        if (1===1)
+        {
+            $output = true;
+        }
+        $this->assertTrue($output);
     }
     /** @test */
     public function checkIfHasKey(): void
