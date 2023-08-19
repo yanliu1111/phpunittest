cb4eac2b19cbe6a2aa3d9d612e8a9d225cc940fb
yanliu1111 <liuyan0504@gmail.com>
2023-08-18 19:11:48 -0600

    refactor: function setUp()

--- tests/unit/EmployeeTest.php
+++ tests/unit/EmployeeTest.php
@@ -5,21 +5,25 @@ use PHPUnit\Framework\TestCase;
 
 class EmployeeTest extends TestCase
 {
+    protected $employee;
+    public function setUp():void{
+        $this -> employee = new \App\Models\Employee;
+
+    }
     public function testGetEmployeeName():void{
-        $employee = new \App\Models\Employee;
-        $employee->setName('Peter');
-        $this->assertEquals($employee->getName(),'Peter');
+        $this -> employee->setName('Peter');
+        $this->assertEquals($this -> employee->getName(),'Peter');
     }
     public function testGetEmployeeAge():void{
-        $employee = new \App\Models\Employee;
-        $employee->setAge(18);
-        $this->assertEquals($employee->getAge(),18);
+        $this -> employee->setAge(18);
+        $this->assertEquals($this -> employee->getAge(),18);
     }
     public function testGetEmployeeNameAndAge():void{
-        $employee = new \App\Models\Employee;
-        $employee->setNameAndAge('Peter',18);
-        $this->assertEquals($employee->getNameAndAge(),['Peter',18]);
+        $this -> employee->setNameAndAge('Peter',18);
+        $this->assertEquals($this -> employee->getNameAndAge(),['Peter',18]);
+        $this -> assertArrayHasKey (1, $this -> employee->getNameAndAge());
     }
+
 }
 
 ?>
